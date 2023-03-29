42c9e809357c1d82177f47e4f2e58ceeb1c26131
codewarrior2022 <nbwebcreations@gmail.com>
2023-03-29 13:49:59 -0500

    Initial Commit

---
+++ FirstApp/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  FirstApp
+//
+//  Created by Nikko Baez on 3/28/23.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye bye, World!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Nikko Baez on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
